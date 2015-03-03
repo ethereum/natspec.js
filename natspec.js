c3d2791007e336a11d20230cdb3bf0b62a88c530
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-03 20:59:46 +0100

    license

--- natspec.js
+++ natspec.js
@@ -1,3 +1,24 @@
+/*
+    This file is part of natspec.js.
+
+    natspec.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    natspec.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with natspec.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/** @file natspec.js
+ * @authors:
+ *   Marek Kotewicz <marek@ethdev.com>
+ * @date 2015
+ */
 
 var abi = require('./node_modules/ethereum.js/lib/abi.js'); 
 
